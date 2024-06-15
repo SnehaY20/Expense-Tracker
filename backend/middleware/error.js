b08b491cf2c2f35f233dbac19efefc0a51d8dbf2
SnehaY20 <21snehay@gmail.com>
2024-06-16 00:36:25 +0530

    Updated the error middleware and added auth middleware

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -17,7 +17,7 @@ const errorHandler = (err, req, res, next) => {
   // Mongoose duplicate key
   if (err.code === 11000) {
     const message = "Duplicate field value entered";
-    error = new ErrorResponse(message, 404);
+    error = new ErrorResponse(message, 400);
   }
 
   // Mongoose validation error
